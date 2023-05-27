fe127d16811d5d1ded5a1cfb2a6e3955d16d4921
hjb22 <hyun.bae22@imperial.ac.uk>
2023-05-27 13:45:42 +0000

    Copied our initial skeleton of emulate

--- src/emulate.c
+++ src/emulate.c
@@ -1,4 +1,33 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+
+int currAddress = 0x0;
+
+/*registers*/
+
+struct pState {
+	bool N;
+	bool Z;
+	bool C;
+	bool V;
+}
+
+struct ZR {
+	int zero;
+}
+
+
+/* Private functions */
+
+static void inc_PC (){
+	currAddress += 4;
+};
+
+static void readInstruction ( int objectCode ) {
+}
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
