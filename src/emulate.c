88656403d8edeec43307f1df4f3a8bf5c0dd390d
hjb22 <hyun.bae22@imperial.ac.uk>
2023-05-29 16:02:26 +0000

    Pull from Master

--- src/emulate.c
+++ src/emulate.c
@@ -1,97 +1,122 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#define NUMREGISTER 31
-#define INITIALADDR 0x0
+/*registers*/
 
-uint64_t generalRegisters[31]; // global var so initialised to 0s
+uint32_t currAddress; // hexadecimal address to represents PC
+
+// ZR always returns 0. No fields needed.
+
+// Initialise number of registers
+#define NUM_REGISTERS 31
+
+uint64_t generalRegisters[NUM_REGISTERS];
+
+// Initialise memory size
+#define MEMORY_SIZE (1024 * 1024)  // 1 MiB, for example
+
+uint8_t memory[MEMORY_SIZE];  // each element represents 1 byte of memory
+// but could also be 2D array?
+
+void initialise_memory() {
+    memset(memory, 0, sizeof(memory));
+} // later
 
 typedef struct {
-	bool N;
-	bool Z;
-	bool C;
-	bool V;
+    bool N;
+    bool Z;
+    bool C;
+    bool V;
 } Pstate;
 
-typedef struct {
-	uint64_t generalRegisters[NUMREGISTER]; //General purpose registers R0-R30
-	uint64_t ZR; //Zero register
-	uint64_t PC; // Program Counter
-	uint64_t SP; //Stack Pointer
-	Pstate PSTATE; //Processor State Register
-} Registers;
-
-void initializeRegister(Registers *machine){
-	for(int i = 0; i < NUMREGISTER; i++){
-		machine->generalRegisters[i] = 0;
-	}
-	machine->ZR = 0;
-	machine->PC = INITIALADDR;
-	machine->SP = 0;
-	machine->PSTATE.N = false;
-	machine->PSTATE.Z = false;
-	machine->PSTATE.C = false;
-	machine->PSTATE.V = false;
-}
-
-typedef enum{
-	DPImm,
-	DPReg,
-	SDT,
-	LL,
-	B
-}instruction;
-
-// global var so initialised to false's
-struct pState PSTATE;
+Pstate pstate;
 
 /* Private functions */
 
-static int extractBits(uint32_t n, int startIndex, int endIndex) {
-	// start/endIndex is inclusive, right-to-left starting from 0
-	int mask = (1 << (endIndex - startIndex + 1)) - 1;
-	return (n >> startIndex) & mask;
+static void inc_PC (){
+    currAddress += 4;
 }
 
-static void inc_PC (Registers *machine){
-	machine->PC += 4;
-}
-
-static instruction readInstruction (uint32_t instruction) {
-	if (extractBits(instruction, 26, 28) == 0b100){
-		return DPImm;
-	}else if (extractBits(instruction, 25, 27) == 0b101)
-	{
-		return DPReg;
-	}else if(extractBits(instruction, 25, 28) == 0b1100 ){
-		if (extractBits(instruction, 31,31) == 1){
-			return SDT;
-		} else {
-			return LL;
-		}
-	} else {
-		return B;
-	}
-}
-
-static int readRegister (int registerIndex) {
-	// registerIndex = 11111 (bin) -> reading from ZR
-	if (registerIndex == 31) {
-		return 0;
-	}
-	return generalRegisters[registerIndex];
+static uint64_t readRegister (int registerIndex) {
+    // registerIndex = 11111 (bin) -> reading from ZR
+    if (registerIndex == 31) {
+        return 0;
+    }
+    return generalRegisters[registerIndex];
 }
 
 static void writeRegister (int registerIndex, int newValue) {
-	// registerIndex = 11111 (bin) -> writing to ZR
-	if (registerIndex == 31) {
-		return;
-	}
-	generalRegisters[registerIndex] = newValue;
+    // registerIndex = 11111 (bin) -> writing to ZR
+    if (registerIndex == 31) {
+        return;
+    }
+    generalRegisters[registerIndex] = newValue;
+}
+
+static int extractBits(uint32_t n, int startIndex, int endIndex) {
+    // start/endIndex is inclusive, right-to-left starting from 0
+    int mask = (1 << (endIndex - startIndex + 1)) - 1;
+    return (n >> startIndex) & mask;
+}
+
+void update_pstate(uint64_t result, uint64_t operand1, uint64_t operand2, bool is_subtraction) {
+    // Update N and Z flags
+    pstate.N = result & (1ULL << 63);  // check the most significant bit
+    pstate.Z = (result == 0);
+
+    if (is_subtraction) {
+        // For subtraction, carry is set if operand1 >= operand2
+        pstate.C = operand1 >= operand2;
+
+        // Overflow for subtraction is set if operand1 and operand2 have different signs,
+        // and operand1 and the result have different signs
+        pstate.V = ((operand1 ^ operand2) & (operand1 ^ result)) >> 63;
+    } else {
+        // For addition, carry is set if result is less than either operand (meaning it wrapped around)
+        pstate.C = result < operand1 || result < operand2;
+
+        // Overflow for addition is set if operand1 and operand2 have the same sign,
+        // and operand1 and the result have different signs
+        pstate.V = (~(operand1 ^ operand2) & (operand1 ^ result)) >> 63;
+    }
+}
+
+static void DPImm(uint32_t instruction) {
+    
+}
+static void DPReg(uint32_t instruction) {
+
+}
+static void SDT(uint32_t instruction) {
+
+}
+static void LL(uint32_t instruction) {
+
+}
+static void B(uint32_t instruction) {
+
+}
+
+/* Decode instruction */
+static void readInstruction (uint32_t instruction) {
+    if (extractBits(instruction, 26, 28) == 0b100){
+        DPImm(instruction);
+    }else if (extractBits(instruction, 25, 27) == 0b101)
+    {
+        DPReg(instruction);
+    }else if(extractBits(instruction, 25, 28) == 0b1100 ){
+        if (extractBits(instruction, 31,31) == 1){
+            SDT(instruction);
+        } else {
+            LL(instruction);
+        }
+    } else {
+        B(instruction);
+    }
 }
 
 int main(int argc, char **argv) {
