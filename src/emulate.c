4c23db7a8bb8805e2cb31068a21198b78b901f3d
Vania <wsc22@aube.doc.ic.ac.uk>
2023-05-30 16:36:27 +0100

    update

--- src/emulate.c
+++ src/emulate.c
@@ -98,7 +98,41 @@ static void LL(uint32_t instruction) {
 
 }
 static void B(uint32_t instruction) {
-
+  #define signExtension(x) ((uint64_t)((uint32_t)x))
+  uint8_t unconditional = extractBits(instruction, 26, 31);
+  uint32_t reg1 = extractBits(instruction, 10, 31);
+  uint8_t reg2 = extractBits(instruction, 0, 4);
+  uint8_t condition1 = extractBits(instruction, 24, 31);
+  uint8_t condition2 = extractBits(instruction, 4, 4);
+  if (unconditional == 0x5) {
+    uint32_t simm26 = extractBits(instruction, 0, 25);
+    uint64_t offset = signExtension(simm26) * 4;
+    PC = PC + offset;
+  } else if (reg2 == 0x0 && reg1 == 0x3508160) {
+    uint8_t xn = extractBits(instruction, 5, 9);
+    PC = readRegister(xn);
+  } else if (condition2 == 0x0 && condition1 == 0x84) {
+    uint32_t simm19 = extractBits(instruction, 5, 23);
+    uint64_t offset = signExtension(simm19) * 4;
+    uint8_t cond = extractBits(instruction, 0, 3);
+    if (cond == 0x0 && pstate.Z == 1) {
+      PC = PC + offset;
+    } else if (cond == 0x1 && pstate.Z == 0) {
+      PC = PC + offset;
+    } else if (cond == 0x6 && pstate.N == 1) {
+      PC = PC + offset;
+    } else if (cond == 0x7 && pstate.N != 1) {
+      PC = PC + offset;
+    } else if (cond == 0x12 && pstate.Z == 0 && pstate.N == pstate.V) {
+      PC = PC + offset;
+    } else if (cond == 0x13 && (pstate.Z == 0 || pstate.N == pstate.V)) {
+      PC = PC + offset;
+    } else if (cond == 0x14) {
+      PC = PC + offset;
+    } 
+  } else if (instruction == 0x3573751839) {
+    PC = PC + 4;
+  }
 }
 
 /* Decode instruction */
