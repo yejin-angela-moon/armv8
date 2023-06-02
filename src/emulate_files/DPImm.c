0a16c7ee05216429e79e2caaa5a1f9f27b5289fd
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-02 13:56:32 +0000

    Update DPImm.c

--- src/emulate_files/DPImm.c
+++ src/emulate_files/DPImm.c
@@ -3,9 +3,10 @@
 static void arithmetic_immediate(uint8_t sf, uint8_t opc, uint32_t operand, uint8_t Rd, state *state)  {
     uint64_t *generalRegisters = state->generalRegisters;
     Pstate pstate = state->pstate;
-    bool sh = (operand >> 17) & 0x01;     // extract bit 22
-    uint16_t imm12 = (operand >> 5) & 0x0FFF; // extract bits 21-10
-    uint8_t rn = operand & 0x1F;             // extract bits 9-5
+
+    bool sh = extractBits(operand, 17, 17);
+    uint16_t imm12 = extractBits(operand, 5, 16);
+    uint8_t rn = extractBits(operand, 0, 4);
     uint64_t result = readRegister(rn, sf, generalRegisters);
 
     if (sh) {
@@ -41,8 +42,9 @@ static void arithmetic_immediate(uint8_t sf, uint8_t opc, uint32_t operand, uint
 
 static void wide_move_immediate(uint8_t sf, uint8_t opc, uint32_t operand, uint8_t Rd, state *state) {
     uint64_t *generalRegisters = state->generalRegisters;
-    unsigned int hw = (operand >> 17) & 0x3;
-    uint16_t imm16 = operand & 0xFFFF;
+
+    uint8_t hw = extractBits(operand, 16, 17);
+    uint16_t imm16 = extractBits(operand, 0, 15);
 
     uint64_t op = ((uint64_t)imm16) << (hw * 16); // calculate op
 
@@ -74,13 +76,13 @@ static void wide_move_immediate(uint8_t sf, uint8_t opc, uint32_t operand, uint8
 void DPImm(uint32_t instruction, state *state) { // data processing instruction (immediate)
     uint8_t sf  = extractBits(instruction, 31, 31);        // extract bit 31
     uint8_t opc = extractBits(instruction, 29, 30);       // extract bits 30-29
-    uint8_t opi = extractBits(instruction, 24, 25);       // extract bits 25-24
+    uint8_t opi = extractBits(instruction, 23, 25);       // extract bits 25-23
     uint32_t operand = extractBits(instruction, 5, 22); // extract bits 22-5
-    uint8_t rd = extractBits(instruction, 0, 4);                // extract bits 4-0
+    uint8_t Rd = extractBits(instruction, 0, 4);
 
     if (opi == 0x2) {
-        arithmetic_immediate(sf, opc, operand, rd, state);
+        arithmetic_immediate(sf, opc, operand, Rd, state);
     } else if (opi == 0x5) {
-        wide_move_immediate(sf, opc, operand, rd, state);
+        wide_move_immediate(sf, opc, operand, Rd, state);
     }
 }
