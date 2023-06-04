dd24b8adc7340a888237738e283e3257744ba31a
Rainie <wsc22@ic.ac.uk>
2023-06-04 23:09:05 +0800

    update 3.4

--- src/emulate_files/B.c
+++ src/emulate_files/B.c
@@ -1,5 +1,62 @@
 #include "B.h"
+#include "B.h"
 
+static void unconditional(uint32_t simm26, state *state) {
+    uint64_t offset = (uint64_t) (simm26 - 1) * 4;
+    state->currAddress += offset;
+}
+
+static void reg(uint8_t xn, state *state) {
+    state->currAddress = readRegister(xn, 1, state->generalRegisters);
+}
+
+static void conditional(uint32_t simm19, uint8_t cond, state *state) {
+    Pstate pstate = state->pstate;
+    uint64_t offset = (int64_t) (simm19 - 1) * 4;
+    if (cond == 0x0 && pstate.Z == 1) {
+        state->currAddress += offset;
+    } else if (cond == 0x1 && pstate.Z == 0) {
+        state->currAddress += offset;
+    } else if (cond == 0x6 && pstate.N == 1) {
+        state->currAddress += offset;
+    } else if (cond == 0x7 && pstate.N != 1) {
+        state->currAddress += offset;
+    } else if (cond == 0xC && pstate.Z == 0 && pstate.N == pstate.V) {
+        state->currAddress += offset;
+    } else if (cond == 0xD && (pstate.Z == 0 || pstate.N == pstate.V)) {
+        state->currAddress += offset;
+    } else if (cond == 0xE) {
+        state->currAddress += offset;
+    }
+}
+
+
+void B(uint32_t instruction, state *state) {
+
+
+    //uint32_t reg1 = extractBits(instruction, 10, 31);
+    //uint8_t reg2 = extractBits(instruction, 0, 4);
+    //uint8_t condition1 = extractBits(instruction, 24, 31);
+    //uint8_t condition2 = extractBits(instruction, 4, 4);
+    uint32_t simm26 = extractBits(instruction, 0, 25);
+    uint8_t xn = extractBits(instruction, 5, 9);
+    uint32_t simm19 = extractBits(instruction, 5, 23);
+    uint8_t cond = extractBits(instruction, 0, 3);
+    if (extractBits(instruction, 26, 31) == 0x5) {
+        //Unconditional
+        unconditional(simm26, state);
+
+    } else if (extractBits(instruction, 0, 4) == 0x0 && extractBits(instruction, 10, 31) == 0x3587c0) {
+        //Register
+        reg(xn, state);
+    } else if (extractBits(instruction, 4, 4) == 0x0 && extractBits(instruction, 24, 31) == 0x54) {
+        //Conditional
+        conditional(simm19, cond, state);
+    }
+}
+
+
+/*
 void B(uint32_t instruction, state *state) {
     Pstate pstate = state->pstate;
 
@@ -38,4 +95,4 @@ void B(uint32_t instruction, state *state) {
             state->currAddress += offset;
         }
     }
-}
+}*/
--- src/test.c
+++ src/test.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 int main() {
-    uint32_t data[] = {0x14000001, 0x91000400, 0x91000c01, 0x8a000000};
+    uint32_t data[] = {0xeb1702ff, 0x54000040, 0x913ee98c, 0x9141098c, 0x8a000000};
     FILE* fp = fopen("test.bin", "wb");
     if (fp == NULL) {
         printf("Failed to create the file. \n");
