c46dbfdff517e0bd35e227aca0102cd13594eabe
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-06 13:29:44 +0000

    Final definitions.h

--- src/emulate_files/definitions.h
+++ src/emulate_files/definitions.h
@@ -11,7 +11,13 @@
 
 #define NUM_REGISTERS 31
 #define HALT_INSTRUCTION 0x8a000000
+#define NOP_INSTRUCTION 0xD503201F
 #define MEMORY_SIZE (2 * 1024 * 1024)  // 2 MiB
+#define LOWER_32_BITS 0xFFFFFFFF
+#define SIGN_EXTEND_32BITS 0xFFFFFFFF00000000
+#define SIGN_EXTEND_19BITS 0xFFFFFFFFFFF80000
+#define SIGN_EXTEND_9BITS 0xFFFFFE00
+#define MASK_1_BYTE 0xFF
 
 typedef struct {
     bool N;
@@ -23,7 +29,7 @@ typedef struct {
 typedef struct {
     Pstate pstate;
     uint64_t generalRegisters[NUM_REGISTERS];
-    uint32_t currAddress; // hexadecimal address to represents PC
+    uint32_t currAddress; // address to represent PC
     uint32_t *memory;
 } state;
 
