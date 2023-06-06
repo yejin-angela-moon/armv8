01569e6264277dba2bfc51d85f50d87fe50b4e81
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-06 13:25:47 +0000

    Final B.h

--- src/emulate_files/B.h
+++ src/emulate_files/B.h
@@ -1,16 +1,13 @@
-#ifndef UNTITLED_B_H
-#define UNTITLED_B_H
+#ifndef B_H
+#define B_H
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include "definitions.h"
 #include "utilities.h"
 
-bool B(uint32_t instruction, state *state);
+void B(uint32_t instruction, state *state);
 
 #endif
