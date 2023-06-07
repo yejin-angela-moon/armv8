d22c21cb1f24ce0a6e76dd92dff542f1b82fd36c
Rainie <wsc22@ic.ac.uk>
2023-06-07 20:16:51 +0800

    final branch with modify

--- src/emulate_files/B.c
+++ src/emulate_files/B.c
@@ -9,10 +9,9 @@ static void modify(int64_t offset, state *state) {
   state->currAddress -= 4;
 }
 
-static void unconditional(uint32_t simm26, state *state) {
+static void unconditional(int64_t simm26, state *state) {
   // b
-  int64_t offset = (int64_t) (simm26 - 1) * 4;
-  state->currAddress += offset;
+  modify(simm26, state);
 }
 
 static void reg(uint8_t xn, state *state) {
@@ -39,10 +38,14 @@ static void conditional(int32_t simm19, uint8_t cond, state *state) {
 }
 
 void B(uint32_t instruction, state *state) {
-  uint32_t simm26 = extractBits(instruction, 0, 25);
+  int64_t simm26 = extractBits(instruction, 0, 25);
   uint8_t xn = extractBits(instruction, 5, 9);
   int32_t simm19 = extractBits(instruction, 5, 23);
   uint8_t cond = extractBits(instruction, 0, 3);
+
+  if (simm26 >> 25) {
+    simm26 += SIGN_EXTEND_26BITS;
+  }
 
   if (simm19 >> 18) {
     simm19 += SIGN_EXTEND_19BITS;
