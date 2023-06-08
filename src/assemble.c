979c57ff8c710f536d568439add3df7ab19d6018
Memi Sawatyanon <ms5022@ic.ac.uk>
2023-06-08 03:06:36 +0100

    assembler layout

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,44 @@
 #include <stdlib.h>
+#include <assert.h>
+#include "assembler_files/definition.h"
+#include "assembler_files/ioutils.h"
+#include "assembler_files/utilities.h"
+
+void parse(row *table, int numLine, char **lines){
+  for (int i =0; i < numLine; i++){
+    if (1){
+        //DPI
+      } else if(1){
+        //DPR
+      } else if(1){
+        //SDT
+      } else if (1){
+        //B
+      } else {
+        //not a instruction
+      }
+  }
+  //Either print as soon as it gets to the instruction
+  //Or store in an output String array then output
+}
+
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+  assert(argc == 3);
+
+  char *inputFile = argv[1];
+  char *outputFile = argv[2];
+
+  int numLine = count_line(inputFile);
+
+  int countLabel = 0;
+  char** lines = readFile(inputFile, numLine, &countLabel);
+
+  row *symbol_table = malloc(sizeof(row) * countLabel);
+
+  parse(symbol_table, numLine, lines);
+
+  free(symbol_table);
+  freeLines(lines, numLine);
+
 }
---
+++ src/assembler_files/utilities.h
@@ -0,0 +1,6 @@
+
+#include <stdbool.h>
+
+int count_lines(char *inputFile);
+bool containColon(char* line);
+void freeLines(char **lines, int numLines);
