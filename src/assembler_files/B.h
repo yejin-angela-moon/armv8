cba16e4dc98b11d0b016427ec530b9977138cfc9
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-12 15:00:33 +0000

    Update B.h

--- src/assembler_files/B.h
+++ src/assembler_files/B.h
@@ -1,14 +1,14 @@
 #ifndef B_H
 #define B_H
 
-#include <stdio.h>
 #include <stdint.h>
-#include <string.h>
-#include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#include "definition.h"
 #include "utilities.h"
 
-uint32_t B(row *table, char **token);
+uint32_t B(row *table, char **token, uint32_t *currAddress);
 
 #endif
