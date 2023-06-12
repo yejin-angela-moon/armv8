f3323c50e9d1818f1567f0f2ca64d5896c8c5a79
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-12 14:59:41 +0000

    Update utilities.h

--- src/assembler_files/utilities.h
+++ src/assembler_files/utilities.h
@@ -1,20 +1,27 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#include <stdbool.h>
+#include <stdio.h>
 #include <stdint.h>
-#include "definition.h"
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <definition.h>
 
 int count_lines(char *inputFile);
 bool containColon(char* line);
 void freeLines(char **lines, int numLines);
 char **tokenizer(char *line, int *numToken);
-uint32_t findAddressTable(char *lable, row *table);
-int getNum(char *string, int start, int size);
-bool isStringInSet(const char *target, const char *set[], size_t setSize);
+static char* decToBinary(uint32_t x, int nbits);
+static uint32_t stringToNumber(char* string);
 char* stringToBinary(char* string, int nbits);
 bool isRegister(const char* reg);
 char* registerToBinary(char* reg);
 char* getSF(const char* reg);
+bool isStringInSet(const char *target, const char *set[], size_t setSize);
+int getNum(char *string, int start, int size);
+uint32_t findAddressTable(char *label, row *table);
 
 #endif
