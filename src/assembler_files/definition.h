19803d04dba31af2c80407d01a23e445a2f6c092
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-12 14:59:22 +0000

    Update definition.h

--- src/assembler_files/definition.h
+++ src/assembler_files/definition.h
@@ -12,14 +12,19 @@ typedef struct {
     uint32_t address;
 } row;
 
-typedef struct {
-    char* name;
-    uint32_t (*func)(char* tokens[], int numOfTokens);
-} func_map;
-
-
 #define MAX_LINE_LENGTH 256
 #define MAX_TOKEN 6
 #define delimiter ", #"
+#define LL_TOKEN_NUM 3
+#define REGISTER_OFFSET 0x81A
+#define LL_INSTRUC 0x18000000
+#define MASK_TO_19BITS 0x7FFFF
+#define SDT_INSTRUC 0xB8000000
+
+const char *dpSet[] = {"add", "adds", "sub", "subs", "movn", "movz", "movk", "and", "bic", "orr", "orn", "eon", "eor", "ands", "bics", "madd", "msub"};
+#define dpSetSize 17
+const char *sdtSet[] = {"ldr", "str"};
+#define sdtSetSize 2
+
 
 #endif
