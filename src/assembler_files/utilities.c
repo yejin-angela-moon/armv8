5c66fe82074f6f826c7f3dc8861db87907539c04
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-13 15:04:12 +0000

    Update utilities.c

--- src/assembler_files/utilities.c
+++ src/assembler_files/utilities.c
@@ -24,98 +24,107 @@ int count_lines(char *inputFile) {
     return linesCount;
 }
 
-bool containColon(char* line) {
-  return(strchr(line, ':') != NULL);
+bool containColon(char *line) {
+    return (strchr(line, ':') != NULL);
 }
 
 
 char **tokenizer(char *line, int *numToken) {
-  int i = 0;
-  char **token = malloc(MAX_TOKEN * sizeof(char*));
-
-  token[0] = strtok(line, delimiter);
-  while (token[i] != NULL){
-    i++;
-    token[i] = strtok(NULL, delimiter);
-  }
-
-  *numToken = i;
-  return token;
+    int i = 0;
+    char **tokens = malloc(MAX_TOKEN * sizeof(char*));
+    tokens[0] = strtok(line, delimiter);
+    while (tokens[i] != NULL) {
+        i++;
+        tokens[i] = strtok(NULL, delimiter);
+    }
+    *numToken = i;
+    return tokens;
 }
 
-void freeLines(char **lines, int numLines){
-  for (int i = 0; i < numLines; i++) {
-    free(lines[i]);
-  }
-  free(lines);
+void freeLines(char **lines, int numLines) {
+    for (int i = 0; i < numLines; i++) {
+        free(lines[i]);
+    }
+    free(lines);
 }
 
 bool isStringInSet(char *target, char *set[], size_t setSize) {
-  for (size_t i = 0; i < setSize; i++) {
-    if (strcmp(target, set[i]) == 0) {
-      return true; // found the string in the set
+    for (size_t i = 0; i < setSize; i++) {
+        if (strcmp(target, set[i]) == 0) {
+            return true; // found the string in the set
+        }
     }
-  }
-  return false; // the string was not found in the set
+    return false; // the string was not found in the set
 }
 
-bool isRegister(const char* reg) {
-  return (reg[0] == 'w' || reg[0] == 'x');
+bool isRegister(const char *reg) {
+    return (reg[0] == 'w' || reg[0] == 'x');
 }
 
-char* decToBinary(uint32_t x, int nbits) {
-  char* res = (char *) malloc(32 * sizeof(char));
-  assert(res != NULL);
-  uint32_t mask = 1 << (nbits - 1);
-  for (int i = 0; i < nbits; i++) {
+char *decToBinary(uint32_t x, int nbits) {
+    char *res = (char *) malloc(32 * sizeof(char));
+    assert(res != NULL);
+    uint32_t mask = 1 << (nbits - 1);
     if ((x & mask) == 0) {
-      strcat(res, "0");
+        strcpy(res, "0");
     } else {
-      strcat(res, "1");
+        strcpy(res, "1");
     }
     mask = mask >> 1;
-  }
-  strcat(res, "\0");
-  return res;
+    for (int i = 1; i < nbits; i++) {
+        if ((x & mask) == 0) {
+            strcat(res, "0");
+        } else {
+            strcat(res, "1");
+        }
+        mask = mask >> 1;
+    }
+    strcat(res, "\0");
+    return res;
 }
 
-uint32_t stringToNumber(char* string) {
-  return (uint32_t)strtol(string, NULL, 0);
+uint32_t stringToNumber(char *string) {
+    return (uint32_t) strtol(string, NULL, 0);
 }
 
-char* stringToBinary(char* string, int nbits) {
-  return decToBinary(stringToNumber(string), nbits);
+
+char *stringToBinary(char *string, int nbits) {
+    return decToBinary(stringToNumber(string), nbits);
 }
 
-char* registerToBinary(char* reg) {
-  // ex: "x11" -> "1011"
-  assert(isRegister(reg));
-  if (strcmp(reg + 1, "zr") == 0) {
-    return "11111";
-  }
-  return decToBinary(stringToNumber(reg + 1), 5);
+uint32_t binaryStringToNumber(char* string) {
+    return (uint32_t) strtol(string, NULL, 2);
 }
 
-char* getSF(const char* reg) {
-  assert(isRegister(reg));
-  return reg[0] == 'w' ? "0" : "1";
+char *registerToBinary(char *reg) {
+    // ex: "x11" -> "1011"
+    assert(isRegister(reg));
+    if (strcmp(reg + 1, "zr") == 0) {
+        return "11111";
+    }
+    return decToBinary(stringToNumber(reg + 1), 5);
+}
+
+char *getSF(const char *reg) {
+    assert(isRegister(reg));
+    return reg[0] == 'w' ? "0" : "1";
 }
 
 int getNum(char *string, int start, int size) {
-  char substring[size];
-  strncpy(substring, string + start, size);
+    char substring[size];
+    strncpy(substring, string + start, size);
 //  substring[--size];
 
-  return (int) stringToNumber(substring);
+    return (int) stringToNumber(substring);
 }
 
 uint32_t findAddressTable(char *label, row *table) {
-  int i = 0;
-  while(table[i].label[0] == '\0'){
-    if (strcmp(table[i].label, label) == 0){
-      return table[i].address;
+    int i = 0;
+    while (table[i].label[0] == '\0') {
+        if (strcmp(table[i].label, label) == 0) {
+            return table[i].address;
+        }
+        i++;
     }
-    i++;
-  }
-  return i;
+    return i;
 }
