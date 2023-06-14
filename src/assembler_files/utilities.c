409608adc254edeee7048f8e0f9c0f42fecac322
Moon, Yejin <yejin.moon22@imperial.ac.uk>
2023-06-14 15:53:55 +0000

    count line function ignores empty line

--- src/assembler_files/utilities.c
+++ src/assembler_files/utilities.c
@@ -4,20 +4,26 @@ int count_lines(char *inputFile) {
   FILE *file;
   int ch;
   int linesCount = 0;
-  bool isLineEmpty = 1;
+  bool nonSpaceCharInLine = false;
 
   file = fopen(inputFile, "r");
   assert(file != NULL);
 
   while ((ch = fgetc(file)) != EOF) {
     if (ch == '\n') {
-      if (!isLineEmpty) {
+      if (nonSpaceCharInLine) {
         linesCount++;
       }
-      isLineEmpty = 1;
-    } else {
-      isLineEmpty = 0;
+      nonSpaceCharInLine = false;
+    } else if (!isspace(ch)) {
+      nonSpaceCharInLine = true;
     }
+  }
+
+  // Handling the case when the file does not end with a newline character,
+  // but the last line contains some non-space characters.
+  if (nonSpaceCharInLine) {
+    linesCount++;
   }
 
   fclose(file);
