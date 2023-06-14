ec394fd07839c57e480d281d3911cf53d588c2e8
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-14 15:57:26 +0000

    Latest readFile

--- src/assembler_files/ioutils.c
+++ src/assembler_files/ioutils.c
@@ -4,35 +4,46 @@
 #include "definition.h"
 #include "utilities.h"
 
-char** readFile(int lineNum, int *countLabel ,char *filename) {
-  FILE *fp = fopen(filename, "r");
+char **readFile(int lineNum, int *countLabel, char *filename) {
+    FILE *file = fopen(filename, "r");
+    assert(file != NULL);
 
-  if (fp == NULL) {
-    fprintf(stderr, "can't open %s\n", filename);
-    exit(1);
-  }
+    char **strings = malloc(lineNum * sizeof(char *));
+    assert(strings != NULL);
 
-  char buffer[MAX_LINE_LENGTH];
-  char **strings = calloc(lineNum, sizeof(char*));
-
-  for (int i = 0; i < lineNum; i++){
-    if (fgets(buffer, sizeof(buffer), fp) != NULL)
-    {
-      if(isspace(buffer[0])){
-        continue;
-      }
-      if (containColon(buffer)){
-        *countLabel++;
-      }
-
-      buffer[strcspn(buffer, "\n")] ='\0';
-      strings[i] = (char*)calloc(MAX_LINE_LENGTH, sizeof(char));
-
-      strcpy(strings[i], buffer);
+    for (int i = 0; i < lineNum; i++) {
+        strings[i] = malloc(MAX_LINE_LENGTH * sizeof(char));
+        assert(strings[i] != NULL);
     }
-  }
 
-  fclose(fp);
+    bool thisLineEmpty = true;
+    bool prevCharNewline = true;
+    int stringIndex = 0;
+    int indexInsideString = 0;
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        if (c != '\n' && !isspace(c)) {
+            thisLineEmpty = false;
+            strings[stringIndex][indexInsideString] = (char) ch;
+            indexInsideString++;
+            prevCharNewline = false;
+        } else if (c == '\n') {
+            indexInsideString = 0;
+            if (!prevCharNewline) {
+                stringIndex++;
+            }
+            prevCharNewline = true;
+        } else { // isspace(c)
+            if (!thisLineEmpty) {
+                strings[stringIndex][indexInsideString] = (char) ch;
+                indexInsideString++;
+            }
+            prevCharNewline = false;
+        }
+    }
+
+    fclose(file);
+    return strings;
 }
 
 void makeSymbolTable(row *table, int lineNum, char **lines){
