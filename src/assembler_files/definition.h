227f797e3f91a9e2c9fb424346c41143a096ea32
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-15 12:22:19 +0000

    Update definition.h

--- src/assembler_files/definition.h
+++ src/assembler_files/definition.h
@@ -7,19 +7,13 @@
 typedef struct {
     char *label;
     uint32_t address;
-} row;
+} symbol_table_row;
 
 #define MAX_LINE_LENGTH 256
-#define MAX_TOKEN 6
+#define MAX_TOKEN 7
 #define delimiter ", #"
 #define REGISTER_OFFSET 0x81A
 #define MASK_TO_19BITS 0x7FFFF
 #define NOP_INSTRUCTION 0xd503201F
-#define MASK_9 0x01FF
-#define B_INSTRUCTION 0x14000000
-#define MASK_TO_26BITS 0x3FFFFFF
-#define BR_INSTRUCTION 0xD61F0000
-#define COND_INSTRUCTION 0x54000000
-
 
 #endif
