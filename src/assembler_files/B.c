5cb46ac1ab44c0745f0b50590340589714dcdd51
Moon, Yejin <yejin.moon22@imperial.ac.uk>
2023-06-15 13:03:16 +0000

    B.c comments and definitions

--- src/assembler_files/B.c
+++ src/assembler_files/B.c
@@ -1,63 +1,69 @@
 #include "B.h"
 #include "utilities.h"
 
-static uint32_t conditional(symbol_table_row *symbol_table, char **tokens, int cond, uint32_t currAddress) {
-    uint32_t instruction = 0x54000000;
-    instruction += cond;
-    // add the address of simm19 << 5
+#define CONDITIONAL_INSTRUCTION 0x54000000
+#define B_INCREMENT 0x14000000
+#define BR_INCREMENT 0xD61F0000
+#define MASK_19_BIT 0x7FFFF
+#define MASK_26_BIT 0x3FFFFFF
 
-    int32_t address;
-    if ((strcmp(tokens[1], "0x0") != 0 || strcmp(tokens[1], "0") != 0) && strtol(tokens[1], NULL, 0) == 0) {
-        address = (int) findAddressOfLabel(tokens[1], symbol_table);
-    } else {
-        address = (int) strtol(tokens[1], NULL, 16);
-    }
+static uint32_t conditional(symbol_table_row *table, char **token, int cond, uint32_t currAddress) {
+  uint32_t instruction = CONDITIONAL_INSTRUCTION; // binary instruction 01010100000000000000000000000000
+  instruction += cond;
+  // add the address of simm19 << 5
 
-    int32_t offset = (int) (address - currAddress) / 4;
-    instruction += (offset & 0x7FFFF) << 5;
-    return instruction;
+  int32_t address;
+  if ((strcmp(token[1], "0x0") != 0 || strcmp(token[1], "0") != 0) && strtol(token[1], NULL, 0) == 0) {
+    address = (int) findAddressOfLabel(token[1], table);
+  } else {
+    address = (int) strtol(token[1], NULL, 16);
+  }
+
+  int32_t offset = (int) (address - currAddress) / 4;
+  instruction += (offset & MASK_19_BIT) << 5; // 01111111111111111111
+  return instruction;
 }
 
-uint32_t B(symbol_table_row *symbol_table, char **tokens, uint32_t currAddress) {
-    uint32_t instruction = 0;
+uint32_t B(symbol_table_row *table, char **token, uint32_t currAddress) {
+  uint32_t instruction = 0;
 
-    if (strcmp(tokens[0], "b") == 0) {
-        instruction += 0x14000000;
+  if (strcmp(token[0], "b") == 0) {
+    instruction += B_INCREMENT; // 00010100000000000000000000000000
 
-        uint32_t address;
+    uint32_t address;
 
-        if ((strcmp(tokens[1], "0x0") != 0 || strcmp(tokens[1], "0") != 0) && strtol(tokens[1], NULL, 0) == 0) {
-            address = findAddressOfLabel(tokens[1], symbol_table);
-        } else {
-            address = strtol(tokens[1], NULL, 16);
-        }
-        int32_t offset = (int) (address - currAddress) / 4;
-        instruction += offset & 0x3FFFFFF;
-
-    } else if (strcmp(tokens[0], "br") == 0) {
-        instruction += 0xD61F0000;
-        // add the register << 5
-        uint32_t reg = registerToBinary(tokens[1]);
-        instruction += reg << 5;
-
+    if ((strcmp(token[1], "0x0") != 0 || strcmp(token[1], "0") != 0) && strtol(token[1], NULL, 0) == 0) {
+      address = findAddressOfLabel(token[1], table);
     } else {
-        char *condStr = strtok(tokens[0], "b.");
-        if (strcmp(condStr, "eq") == 0) {
-            return conditional(symbol_table, tokens, 0, currAddress);
-        } else if (strcmp(condStr, "ne") == 0) {
-            return conditional(symbol_table, tokens, 1, currAddress);
-        } else if (strcmp(condStr, "ge") == 0) {
-            return conditional(symbol_table, tokens, 0xA, currAddress);
-        } else if (strcmp(condStr, "lt") == 0) {
-            return conditional(symbol_table, tokens, 0xB, currAddress);
-        } else if (strcmp(condStr, "gt") == 0) {
-            return conditional(symbol_table, tokens, 0xC, currAddress);
-        } else if (strcmp(condStr, "le") == 0) {
-            return conditional(symbol_table, tokens, 0xD, currAddress);
-        } else if (strcmp(condStr, "al") == 0) {
-            return conditional(symbol_table, tokens, 0xE, currAddress);
-        }
+      address = strtol(token[1], NULL, 16); // string to hex
     }
+    int32_t offset = (int) (address - currAddress) / 4;
+    instruction += offset & MASK_26_BIT; // 0011111111111111111111111111
 
-    return instruction;
+  } else if (strcmp(token[0], "br") == 0) {
+    instruction += BR_INCREMENT; // 11010110000111110000000000000000
+    // add the register << 5
+    uint32_t reg = registerToBinary(token[1]);
+    instruction += reg << 5;
+
+  } else {
+    char *condStr = strtok(token[0], "b.");
+    if (strcmp(condStr, "eq") == 0) {
+      return conditional(table, token, 0, currAddress);
+    } else if (strcmp(condStr, "ne") == 0) {
+      return conditional(table, token, 1, currAddress);
+    } else if (strcmp(condStr, "ge") == 0) {
+      return conditional(table, token, 0xA, currAddress);
+    } else if (strcmp(condStr, "lt") == 0) {
+      return conditional(table, token, 0xB, currAddress);
+    } else if (strcmp(condStr, "gt") == 0) {
+      return conditional(table, token, 0xC, currAddress);
+    } else if (strcmp(condStr, "le") == 0) {
+      return conditional(table, token, 0xD, currAddress);
+    } else if (strcmp(condStr, "al") == 0) {
+      return conditional(table, token, 0xE, currAddress);
+    }
+  }
+
+  return instruction;
 }
