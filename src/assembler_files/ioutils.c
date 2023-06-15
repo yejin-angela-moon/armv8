5f90f2b86e7f259eeb82c62ed99394c800f2c18f
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-15 12:22:39 +0000

    Update ioutils.c

--- src/assembler_files/ioutils.c
+++ src/assembler_files/ioutils.c
@@ -1,65 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 #include "utilities.h"
 
 char **readFile(int lineNum, int *countLabel, char *filename) {
-  FILE *file = fopen(filename, "r");
-  assert(file != NULL);
+    FILE *file = fopen(filename, "r");
+    assert(file != NULL);
 
-  char **strings = malloc(lineNum * sizeof(char *));
-  assert(strings != NULL);
-
-  for (int i = 0; i < lineNum; i++) {
-    strings[i] = malloc(MAX_LINE_LENGTH * sizeof(char));
-    assert(strings[i] != NULL);
-  }
-
-  bool thisLineEmpty = true;
-//  bool prevCharNewline = true;
-  int stringIndex = 0;
-  int indexInsideString = 0;
-  int c;
-  while ((c = fgetc(file)) != EOF) {
-    if (c != '\n' && !isspace(c)) {
-      thisLineEmpty = false;
-      strings[stringIndex][indexInsideString] = (char) c;
-      indexInsideString++;
-//      prevCharNewline = false;
-    } else if (c == '\n') {
-      if (!thisLineEmpty) {
-        strings[stringIndex][indexInsideString] = '\0';
-        stringIndex++;
-      }
-      indexInsideString = 0;
-      thisLineEmpty = true;
-//      prevCharNewline = true;
-    } else { // isspace(c)
-      if (!thisLineEmpty) {
-        strings[stringIndex][indexInsideString] = (char) c;
-        indexInsideString++;
-      }
-//      prevCharNewline = false;
+    char **strings = malloc(lineNum * sizeof(char *));
+    assert(strings != NULL);
+    for (int i = 0; i < lineNum; i++) {
+        strings[i] = malloc(MAX_LINE_LENGTH * sizeof(char));
+        assert(strings[i] != NULL);
     }
-  }
 
-  fclose(file);
-  return strings;
+    int ch;
+    bool isLineEmpty = true;
+    int stringIndex = 0;
+    int indexInsideString = 0;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == ':') {
+            (*countLabel)++;
+        }
+
+        if (ch == '\n') {
+            if (!isLineEmpty) {
+                strings[stringIndex][indexInsideString] = '\0';
+                stringIndex++;
+            }
+            isLineEmpty = true;
+            indexInsideString = 0;
+        } else {
+            isLineEmpty = false;
+            strings[stringIndex][indexInsideString] = (char) ch;
+            indexInsideString++;
+        }
+    }
+
+    fclose(file);
+    return strings;
 }
 
+void makeSymbolTable(symbol_table_row *symbol_table, int lineNum, char **lines) {
+    int j = 0;
+    int addr = 0;
 
-void makeSymbolTable(row *table, int lineNum, char **lines) {
-  int j = 0;
-  int addr = 0;
-  for (int i = 0; i < lineNum; i++) {
-    if (containColon(lines[i])) {
-      table[j].address = addr * 4;
-      deleteColon(lines[i], strlen(lines[i]));
-      table[j].label = lines[i];
-      j++;
-    } else {
-      addr++;
+    for (int i = 0; i < lineNum; i++) {
+        if (containsColon(lines[i])) {
+            symbol_table[j].address = addr * 4;
+
+            deleteColon(lines[i], strlen(lines[i]));
+            // deletes colon from every line with label
+
+            symbol_table[j].label = lines[i];
+            j++;
+        } else if (strlen(lines[i]) > 2) {
+            addr++;
+        }
     }
-  }
 }
