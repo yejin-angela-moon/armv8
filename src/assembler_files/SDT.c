7b5ca76b1c9bbd4a924fc0b1c83cbd6f6e3fc6c2
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-15 12:29:20 +0000

    Update SDT.c

--- src/assembler_files/SDT.c
+++ src/assembler_files/SDT.c
@@ -1,30 +1,30 @@
 #include "SDT.h"
 #include "definition.h"
 
-static uint32_t preIndexed(char **token) {
+static uint32_t preIndexed(char **tokens) {
 
     uint32_t instruction = 0;
-    if (token[1][0] == 'x') {
+    if (tokens[1][0] == 'x') {
         instruction |= 1 << 30;
     }
 
-    int rt = getSubstringAsInt(token[1], 1, 2);
+    int rt = getSubstringAsInt(tokens[1], 1, 2);
     instruction |= rt;
 
     instruction |= SDT_INSTRUC;
 
-    if (strcmp(token[0], "ldr") == 0) {
+    if (strcmp(tokens[0], "ldr") == 0) {
         //ldr
         instruction |= 1 << 22;
     }
-    int xn = getSubstringAsInt(token[2], 2, 2);
+    int xn = getSubstringAsInt(tokens[2], 2, 2);
     instruction |= xn << 5;
 
     instruction |= 1 << 10;
     instruction |= 1 << 11;
 
-    int32_t simm = (int) strtol(token[3], NULL, 0);
-    if (strchr(token[3], '-') != NULL) {
+    int32_t simm = (int) strtol(tokens[3], NULL, 0);
+    if (strchr(tokens[3], '-') != NULL) {
         simm = (~simm) + 1;
     }
 
@@ -32,29 +32,29 @@ static uint32_t preIndexed(char **token) {
     return instruction;
 }
 
-static uint32_t postIndexed(char **token, int countToken) {
+static uint32_t postIndexed(char **tokens) {
     uint32_t instruction = 0;
-    if (token[1][0] == 'x') {
+    if (tokens[1][0] == 'x') {
         instruction |= 1 << 30;
     }
 
-    int rt = getSubstringAsInt(token[1], 1, 2);
+    int rt = getSubstringAsInt(tokens[1], 1, 2);
     instruction |= rt;
 
     instruction |= SDT_INSTRUC;
 
 
-    if (strcmp(token[0], "ldr") == 0) {
+    if (strcmp(tokens[0], "ldr") == 0) {
         instruction |= 1 << 22;
     }
 
-    int xn = getSubstringAsInt(token[2], 2, 2);
+    int xn = getSubstringAsInt(tokens[2], 2, 2);
     instruction |= xn << 5;
     instruction |= 1 << 10;
 
     int16_t simm;
-    simm = (int16_t) strtol(strtok(token[3], "-"), NULL, 10);
-    if (strchr(token[3], '-') != NULL) {
+    simm = (int16_t) strtol(strtok(tokens[3], "-"), NULL, 10);
+    if (strchr(tokens[3], '-') != NULL) {
         simm = (int16_t) ((~simm) + 1);
     }
 
@@ -62,75 +62,75 @@ static uint32_t postIndexed(char **token, int countToken) {
     return instruction;
 }
 
-static uint32_t registerOffset(char **token) {
+static uint32_t registerOffset(char **tokens) {
 
     uint32_t instruction = 0;
-    if (tolower(token[1][0]) == 'x') {
+    if (tolower(tokens[1][0]) == 'x') {
         instruction |= 1 << 30;
     }
 
-    int rt = getSubstringAsInt(token[1], 1, 2);
+    int rt = getSubstringAsInt(tokens[1], 1, 2);
     instruction |= rt;
     instruction |= SDT_INSTRUC;
 
 
-    if (strcmp(token[0], "ldr") == 0) {
+    if (strcmp(tokens[0], "ldr") == 0) {
         instruction |= 1 << 22;
     }
-    int xn = getSubstringAsInt(token[2], 2, 2);
+    int xn = getSubstringAsInt(tokens[2], 2, 2);
     instruction |= xn << 5;
 
     instruction |= REGISTER_OFFSET << 10;
 
-    int xm = getSubstringAsInt(token[3], 1, 2);
+    int xm = getSubstringAsInt(tokens[3], 1, 2);
     instruction |= xm << 16;
     return instruction;
 }
 
-static uint32_t unsignedOffset(char **token, int countToken) {
+static uint32_t unsignedOffset(char **tokens, int countToken) {
     uint32_t instruction = 0;
-    if (token[1][0] == 'x') {
+    if (tokens[1][0] == 'x') {
         instruction |= 1 << 30;
     }
 
-    int rt = getSubstringAsInt(token[1], 1, 2);
+    int rt = getSubstringAsInt(tokens[1], 1, 2);
     instruction |= rt;
 
     instruction |= SDT_INSTRUC;
 
 
-    if (strcmp(token[0], "ldr") == 0) {
+    if (strcmp(tokens[0], "ldr") == 0) {
         instruction |= 1 << 22;
     }
-    int xn = getSubstringAsInt(token[2], 2, 2);
+    int xn = getSubstringAsInt(tokens[2], 2, 2);
     instruction |= xn << 5;
 
 
     instruction |= 1 << 24;
 
     if (countToken > 3) {
-        if (tolower(token[1][0]) == 'x') {
-            instruction |= strtol(token[3], NULL, 0) / 8 << 10;
+        if (tolower(tokens[1][0]) == 'x') {
+            instruction |= strtol(tokens[3], NULL, 0) / 8 << 10;
         } else {
-            instruction |= strtol(token[3], NULL, 0) / 4 << 10;
+            instruction |= strtol(tokens[3], NULL, 0) / 4 << 10;
         }
     }
 
     return instruction;
 }
 
-static uint32_t LL(char **token, symbol_table_row *table, uint32_t currAddress) {
+static uint32_t LL(char **tokens, symbol_table_row *symbol_table, uint32_t currAddress) {
     uint32_t instruction = 0;
-    if (tolower(token[1][0]) == 'x') {
+    if (tolower(tokens[1][0]) == 'x') {
         instruction |= 1 << 30;
     }
 
-    int rt = getSubstringAsInt(token[1], 1, 2);
+    int rt = getSubstringAsInt(tokens[1], 1, 2);
     instruction |= rt;
 
     instruction |= LL_INSTRUC;
 
-    uint32_t addressLabel = findAddressOfLabel(token[2], table);
+    uint32_t addressLabel = findAddressOfLabel(tokens[2], symbol_table);
 
     int32_t offset = (int) (addressLabel - currAddress) / 4;
     instruction |= (offset & MASK_TO_19BITS) << 5;
@@ -138,21 +138,21 @@ static uint32_t LL(char **token, symbol_table_row *table, uint32_t currAddress) 
     return instruction;
 }
 
-uint32_t mode(char **token, int countToken) {
-    if (countToken >= 4 && strchr(token[3], '!') != NULL) {
-        return preIndexed(token);
-    } else if (countToken >= 4 && strchr(token[2], ']') != NULL) {
-        return postIndexed(token, countToken);
-    } else if (countToken >= 4 && isRegister(strtok(token[3], "]"))) {
-        return registerOffset(token);
+static uint32_t mode(char **tokens, int countToken) {
+    if (countToken >= 4 && strchr(tokens[3], '!') != NULL) {
+        return preIndexed(tokens);
+    } else if (countToken >= 4 && strchr(tokens[2], ']') != NULL) {
+        return postIndexed(tokens);
+    } else if (countToken >= 4 && isRegister(strtok(tokens[3], "]"))) {
+        return registerOffset(tokens);
     } else {
-        return unsignedOffset(token, countToken);
+        return unsignedOffset(tokens, countToken);
     }
 }
 
-uint32_t SDT(char **token, symbol_table_row *table, int countToken, uint32_t currAddress) {
-    if (strchr(token[2], '[') == NULL) {
-        return LL(token, table, currAddress);
+uint32_t SDT(char **tokens, symbol_table_row *symbol_table, int countToken, uint32_t currAddress) {
+    if (strchr(tokens[2], '[') == NULL) {
+        return LL(tokens, symbol_table, currAddress);
     }
-    return mode(token, countToken);
+    return mode(tokens, countToken);
 }
