8dc470f8d654956941a51bc51c86a348e2a54fe6
hjb22 <hyun.bae22@imperial.ac.uk>
2023-06-15 12:26:12 +0000

    Update SDT.h

--- src/assembler_files/SDT.h
+++ src/assembler_files/SDT.h
@@ -8,7 +8,7 @@
 #define SDT_INSTRUC 0xB8000000
 #define LL_INSTRUC 0x18000000
 
-uint32_t SDT(char **token, row *table, int countToken, uint32_t currAddress);
+uint32_t SDT(char **tokens, symbol_table_row *symbol_table, int countToken, uint32_t currAddress);
 
 #include "utilities.h"
 
